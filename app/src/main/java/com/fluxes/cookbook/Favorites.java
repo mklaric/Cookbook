f10914418b090e9c2e130a06bc76757254890afb
Magda Klaric <magda.klaric@gmail.com>
2017-02-22 23:38:45 +0100

    Added description to NewRecipe activity & dynamically showing favorite recepies from server

--- app/src/main/java/com/fluxes/cookbook/Favorites.java
+++ app/src/main/java/com/fluxes/cookbook/Favorites.java
@@ -1,29 +1,76 @@
 package com.fluxes.cookbook;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.RatingBar;
 import android.widget.TableLayout;
+import android.widget.TextView;
+
+import org.json.JSONException;
 
 public class Favorites extends AppCompatActivity {
+
+    public static final String MYPREFS= "token_prefs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites);
 
-        TableLayout recipes= (TableLayout) findViewById(R.id.favsTable);
-        View ingrow = getLayoutInflater().inflate(R.layout.recipe_info_row, null,false);
-        View ingrow2 = getLayoutInflater().inflate(R.layout.recipe_info_row, null,false);
-        View ingrow3 = getLayoutInflater().inflate(R.layout.recipe_info_row, null,false);
+        SharedPreferences mySharedPreferences=getSharedPreferences(MYPREFS,MODE_PRIVATE);
+        String token=mySharedPreferences.getString("access_token", "");
+        new FavoritesRequest().execute(ServerAPI.GetFavorites(token));
 
-        recipes.addView(ingrow);
-        recipes.addView(ingrow2);
-        recipes.addView(ingrow3);
+    }
 
+    private class FavoritesRequest extends SendRequest {
+        @Override
+        protected void onPostExecute(Request result) {
+
+            try {
+                String Title = null;
+                int numberOfRecipes= result.Response().length();
+
+                View rows[]=new View[numberOfRecipes];
+                TableLayout recipes= (TableLayout) findViewById(R.id.favsTable);
+
+                for (int i = 0; i < numberOfRecipes; ++i) {
+
+                    Title = result.Response().getJSONObject(i).getString("name");
+                    String Description = result.Response().getJSONObject(i).getString("description");
+                    float Grade=(float) result.Response().getJSONObject(i).getDouble("grade");
+                    int id=result.Response().getJSONObject(i).getInt("id");
+
+
+                    rows[i]=getLayoutInflater().inflate(R.layout.recipe_info_row, null,false);
+                    TextView title= (TextView) rows[i].findViewById(R.id.recInfoTitle);
+                    TextView description = (TextView) rows[i].findViewById(R.id.recInfoDescription);
+                    RatingBar rating = (RatingBar) rows[i].findViewById(R.id.recInfoRating);
+
+
+                    title.setText(Title);
+                    title.setTag(id);
+                    description.setText(Description);
+                    rating.setRating(Grade);
+
+                    recipes.addView(rows[i]);
+                }
+                Log.d("FILTER", result.Response().toString());
+            }
+            catch (JSONException e){
+
+            }
+
+        }
     }
 
     @Override
